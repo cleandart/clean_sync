ef0da06940d529f9b0e5da23bbff1854e52b4ec8
Marcela Hrda <aklecram@gmail.com>
2013-11-07 21:12:44 +0100

    publisher contains a class and creates one default instance for convenient usage

--- lib/src/publisher.dart
+++ lib/src/publisher.dart
@@ -7,42 +7,50 @@ part of clean_sync_server;
 typedef DataProvider DataGenerator(Map args);
 Map<String, DataGenerator> _publishedCollections = {};
 
-void publish(String collection, DataGenerator callback) {
-  _publishedCollections[collection] = callback;
+class Publisher {
+  void publish(String collection, DataGenerator callback) {
+    _publishedCollections[collection] = callback;
+  }
+
+  bool isPublished(String collection) {
+    return _publishedCollections.keys.contains(collection);
+  }
+
+  Future handleSyncRequest(request) {
+    print('lala');
+    Map data = request['args'];
+    print("REQUEST:  ${data}");
+
+    DataProvider dp = _publishedCollections[data['collection']](data['args']);
+
+    if (data["action"] == "get_data") {
+      return dp.data();
+    }
+    else if (data["action"] == "get_diff") {
+      return dp.diffFromVersion(data["version"]);
+    }
+    else if (data["action"] == "add") {
+      return dp.add(data['_id'], data['data'], data['author']);
+    }
+    else if (data["action"] == "change") {
+      return dp.change(data['_id'], data['data'], data['author']);
+    }
+    else if (data["action"] == "remove") {
+      return dp.remove(data['_id'], data['author']);
+    }
+  // return new Future.value({"action" : data["action"]});
+  }
+}
+
+var default_publisher = new Publisher();
+void publish(String c, DataGenerator dg) {
+  default_publisher.publish(c, dg);
 }
 
 bool isPublished(String collection) {
-  return _publishedCollections.keys.contains(collection);
+  return default_publisher.isPublished(collection);
 }
 
-Future _handleSyncRequest(request) {
-  Map data = request['request']['args'];
-  print("REQUEST:  ${data}");
-
-  DataProvider dp = _publishedCollections[data['collection']](data['args']);
-
-
-  if (data["action"] == "get_data") {
-//      num version = mongo.maxVersion;
-    return dp.data();//.then((d) => {"data" : d, "version" : version});
-  }
-  else if (data["action"] == "get_diff") {
-    return dp.diffFromVersion(data["version"]);
-  }
-  else if (data["action"] == "add") {
-    return dp.add(data['_id'], data['data'], data['author']);
-    //return mongo.collection(data["collection"]).add(data["data"], data["author"]);
-  }
-  else if (data["action"] == "change") {
-    return dp.change(data['_id'], data['data'], data['author']);
-//      return mongo.collection(data["collection"]).change(data["_id"], data["data"], data["author"]);
-  }
-  else if (data["action"] == "remove") {
-    return dp.remove(data['_id'], data['author']);
-//      return mongo.collection(data["collection"]).remove(data["_id"], data["author"]);
-  }
-
-  // return new Future.value({"action" : data["action"]});
+Future handleSyncRequest(request) {
+  return default_publisher.handleSyncRequest(request);
 }
-
-Function createSyncRequestHandler = (request) => _handleSyncRequest(request);
