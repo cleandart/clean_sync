a17aa0ba7dd5b2126ee07eb9ca97ea056462cc77
Marcela Hrda <aklecram@gmail.com>
2013-11-08 11:15:46 +0100

    pridany Igorov test na ukazku synchronizacie

---
+++ bin/example_server.dart
@@ -0,0 +1,19 @@
+import 'package:clean_sync/clean_sync_server.dart';
+import 'package:clean_backend/clean_backend.dart';
+import 'package:static_file_handler/static_file_handler.dart';
+
+void main(){
+  var mongo = new MongoProvider('mongodb://clean:clean@127.0.0.1:27017/clean');
+  publish('persons', (_) {
+    return mongo.collection("persons");
+  });
+    publish('personsOlderThan24', (_) {
+    return mongo.collection("persons").find({"age" : {'\$gt' : 24}});
+  });
+  StaticFileHandler fileHandler = new StaticFileHandler.serveFolder('/home/marcelka/');
+  RequestHandler requestHandler = new RequestHandler();
+  requestHandler.registerExecutor('', handleSyncRequest);
+  mongo.initialize(['persons']).then((_) {
+      new Backend(fileHandler, requestHandler, host: '127.0.0.1', port: 8080)..listen();
+  });
+}
