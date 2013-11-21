5fd39351a91d861092afa12c6fc732198e2b4c54
mama <tomas.kulich@gmail.com>
2013-11-21 23:46:53 +0100

    adjust to new backend

--- bin/example_server.dart
+++ bin/example_server.dart
@@ -2,6 +2,8 @@ import 'package:clean_sync/server.dart';
 import 'package:clean_backend/clean_backend.dart';
 import 'dart:async';
 import 'package:clean_ajax/server.dart';
+import 'package:crypto/crypto.dart';
+
 
 
 void main() {
@@ -20,17 +22,22 @@ void main() {
     });
     publish('personsOlderThan24', (_) {
       return mongodb.collection("persons").find({"age" : {'\$gt' : 24}});
+//      return mongodb.collection("persons").find({"age" : null});
     });
 
-    var backend = new Backend(host: '127.0.0.1', port: 8080);
-    backend.listen().then((_) {
-      backend.addDefaultHttpHeader('Access-Control-Allow-Origin','*');
-
+    Backend.bind([], new SHA256()).then((backend) {
       MultiRequestHandler requestHandler = new MultiRequestHandler();
       requestHandler.registerDefaultHandler(handleSyncRequest);
-
+      backend.addDefaultHttpHeader('Access-Control-Allow-Origin','*');
       backend.addView(r'/resources', requestHandler.handleHttpRequest);
       backend.addStaticView(new RegExp(r'/.*'), '../web/');
     });
+
+//    var backend = new Backend(host: '127.0.0.1', port: 8080);
+//    backend.listen().then((_) {
+//
+//
+//      backend.addView(r'/resources', requestHandler.handleHttpRequest);
+//    });
   });
 }
