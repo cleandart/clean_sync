065eca65f150f5fe2b226da81be8f18ce2643943
mama <tomas.kulich@gmail.com>
2014-01-23 16:37:40 +0100

    indexing on _id automatically

--- lib/src/subscription.dart
+++ lib/src/subscription.dart
@@ -120,6 +120,7 @@ class Subscription {
   Subscription(this.collectionName, this._connection, this._author,
       this._idGenerator, [this.args]) {
     collection = new DataSet();
+    collection.addIndex(['_id']);
     start();
   }
 
