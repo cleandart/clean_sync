c5d643c36f1816d9e259e7ee988da3ba5a0ebbfb
mama <tomas.kulich@gmail.com>
2014-02-14 19:36:17 +0100

    in progress

--- lib/src/cache.dart
+++ lib/src/cache.dart
@@ -2,27 +2,36 @@
 import 'dart:collection';
 
 class Entry {
-  num timeAdded;
+  DateTime timeAdded;
+  DateTime expirationDate;
   dynamic value;
+
+  Entry(this.value, this.timeAdded, this.expirationDate){
+  }
 }
 
 class Cache {
   Map m;
-  num _timeOut;
+  Duration _timeOut;
   LinkedHashMap<dynamic, Entry> entries;
 
   Cache(this._timeOut);
 
   clear(){
+    var toRemove = [];
     for(var k in entries.keys){
-      if (entries[k].timeAdded) {
-
+      if ((new DateTime.now()).isAfter(entries[k].expirationDate)) {
+        toRemove.add(k);
       }
     }
+    toRemove.forEach((e){entries.remove(e);});
   }
 
-
   save(key, val){
+    var timeAdded = new DateTime.now();
+    var expirationDate = timeAdded.add(_timeOut);
+
+    entries[key] = new Entry(value);
 
   }
 }
