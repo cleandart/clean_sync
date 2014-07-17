976467fa218e7a1cc5f5ffc159bebeb5e610c1c4
Jozef Brandys <jozef.brandys@gmail.com>
2014-07-17 19:28:26 +0200

    dummy test for mongo error update

--- test/test.dart
+++ test/test.dart
@@ -15,15 +15,14 @@ import 'package:clean_sync/mongo_server.dart';
 import 'package:clean_sync/mongo_client.dart';
 import 'package:clean_sync/id_generator.dart';
 
-int ab = 35;
+int ab = 1;
 var selectors = [
-{"\$query":{"\$or":[{"\$and":[{"version":{"\$gt":ab}},{"before":{"\$gt":{}}},{"before.a":"hello"}]},{"\$and":[{"version":{"\$gt":ab}},{"after":{"\$gt":{}}},{"after.a":"hello"}]}],"version":{"\$gt":ab}}},
-{"\$query":{"\$or":[{"\$and":[{"version":{"\$gt":ab}},{"before":{"\$gt":{}}},{"before.a":"hello"}]},{"\$and":[{"version":{"\$gt":ab}},{"after":{"\$gt":{}}},{"after.a":"hello"}]}],"version":{"\$gt":ab}},"\$orderby":{"version":1}}
+ { 'name': 'Excepted result...', 'selector': {"\$query":{"\$or":[{"\$and":[{"version":{"\$gt":ab}},{"before":{"\$gt":{}}},{"before.a":"hello"}]},{"\$and":[{"version":{"\$gt":ab}},{"after":{"\$gt":{}}},{"after.a":"hello"}]}],"version":{"\$gt":ab}}}},
+ { 'name': 'Actual result...', 'selector': {"\$query":{"\$or":[{"\$and":[{"version":{"\$gt":ab}},{"before":{"\$gt":{}}},{"before.a":"hello"}]},{"\$and":[{"version":{"\$gt":ab}},{"after":{"\$gt":{}}},{"after.a":"hello"}]}],"version":{"\$gt":ab}},"\$orderby":{"version":1}}}
 ];
 
 
 main() {
-
   MongoDatabase mongodb;
   MongoServer mongoServer = new MongoServer(27001, "mongodb://0.0.0.0/mongoProviderTest");
   return mongoServer.start()
@@ -31,16 +30,16 @@ main() {
         mongodb = mongoServer.db;
         MongoProvider provider = mongodb.collection("random");
         return Future.forEach(selectors, (selector) {
-            return provider.collectionHistory.find(selector).toList()
+            return provider.collectionHistory.find(selector['selector']).toList()
                .then((data) {
-                  print(selector);
+                  print(selector['name']);
                   print('');
 
                   data.forEach((e) => print(e));
 
                   print('');
               });
-        });
+        }).then((_) => print('QUERY TO EXPLORE:\n${selectors[1]['selector']}'));
   }).then((_) => mongoServer.close());
 
 }
