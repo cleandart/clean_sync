3566fc977be8cc54328f2d40699a74521caed021
jurom <jurajmchc@gmail.com>
2014-09-04 17:37:15 +0200

    installation script changes

--- tool/installLocker.sh
+++ tool/installLocker.sh
@@ -1,6 +1,6 @@
-sync=`pwd | sed -e "s/\(.*clean_sync\).*/\1/g"`
-p=`find ${sync} | grep mongo_locker\.dart$`
-tmp=`find ${sync} | grep upstart_template$`
-cat ${tmp} | sed -e "s|\(.*\)__PATH_TO_LOCKER__\(.*\)|\1${p}\2|g" > /etc/init/locker.conf
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+p="${DIR}/../bin/mongo_locker.dart"
+cat upstart_template | sed -e "s|__PATH_TO_LOCKER__|${p}|g" > /etc/init/locker.conf
 sudo initctl reload-configuration
 sudo service locker restart
