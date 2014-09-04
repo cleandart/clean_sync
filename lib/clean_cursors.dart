825166f3c4b5005c01473e8142aed879026d143b
Jozef Brandys <jozef.brandys@gmail.com>
2014-09-04 21:32:29 +0200

    refFromCursor

--- lib/clean_cursors.dart
+++ lib/clean_cursors.dart
@@ -4,9 +4,16 @@ import 'package:persistent/persistent.dart';
 import 'package:persistent/persistent.dart' as per;
 import 'dart:async';
 import 'dart:collection';
+import 'package:clean_data/clean_data.dart';
 
 part 'src_cursors/cursor.dart';
 part 'src_cursors/map_cursor.dart';
 part 'src_cursors/reference.dart';
 part 'src_cursors/list_cursor.dart';
 part 'src_cursors/set_cursor.dart';
+
+DataReference refFromCursor(Cursor cursor) {
+  DataReference ref = new DataReference(cursor.value);
+  cursor.onChange.listen((_) => ref.value = cursor.value);
+  return ref;
+}
